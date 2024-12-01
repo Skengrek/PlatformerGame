8663217fc3dd422d6b3e8ec4f4010190a304958b
Skengrek <armand.masse69@gmail.com>
2024-12-01 22:07:44 +0100

    Added a simple tileset

--- world.gd
+++ world.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
-@onready var collision_polygon_2d: CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
-@onready var polygon_2d: Polygon2D = $StaticBody2D/CollisionPolygon2D/Polygon2D
 @onready var level_completed: ColorRect = $CanvasLayer/LevelCompleted
 
 func _ready():
-	polygon_2d.polygon = collision_polygon_2d.polygon
 	Events.level_completed.connect(show_level_completed)
 	
 
