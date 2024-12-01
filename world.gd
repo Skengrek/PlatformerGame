1a92e4c968e57563456fcbbab886f2d22f6ca890
Skengrek <armand.masse69@gmail.com>
2024-12-01 15:43:13 +0100

    Second commit

---
+++ world.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var collision_polygon_2d: CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
+@onready var polygon_2d: Polygon2D = $StaticBody2D/CollisionPolygon2D/Polygon2D
+@onready var level_completed: ColorRect = $CanvasLayer/LevelCompleted
+
+func _ready():
+	polygon_2d.polygon = collision_polygon_2d.polygon
+	Events.level_completed.connect(show_level_completed)
+	
+
+func show_level_completed():
+	level_completed.show()
+	get_tree().paused = true
